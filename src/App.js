99bc7efc2ff0b9a189ab8de3cf374e273228cc2d
Appelbaumgl <Appelbaumgl@msoe.edu>
2018-12-12 16:38:32 -0600

    Separated the update events

--- src/App.js
+++ src/App.js
@@ -9,9 +9,19 @@ class App extends Component {
     //Sets up the SignalR connection
     var connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:44352/hub").build();
 
-    //Called whenever an update is recieved from the API.
-    connection.on("Update", function (message) {
-      console.log(message);
+    //Called whenever an appointment update is recieved from the API.
+    connection.on("AppointmentUpdate", function (message) {
+      console.log("Appointment", message);
+    });
+
+    //Called whenever a service tech update is recieved from the API.
+    connection.on("ServiceTechUpdate", function (message) {
+      console.log("ServiceTech", message);
+    });
+
+    //Called whenever a company update is recieved from the API.
+    connection.on("CompanyUpdate", function (message) {
+      console.log("Company", message);
     });
       
     //Starts the connection and adds the connection to a group after the connection is established.
